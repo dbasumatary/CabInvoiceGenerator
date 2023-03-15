dd1cdc2a25ca02b055ffa8fb46e2fc8e34fb261e
dbasumatary <basudhondip@gmail.com>
2023-03-15 23:45:51 +0530

    Added: Test cases to calculate total fare for multiple rides

--- src/cabinvoicegenerator/test/InvoiceGeneratorTest.java
+++ src/cabinvoicegenerator/test/InvoiceGeneratorTest.java
@@ -1,12 +1,15 @@
 package cabinvoicegenerator.test;
 import cabinvoicegenerator.main.InvoiceGenerator;
+import cabinvoicegenerator.main.RideDetail;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    public static InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
+    //UC1 - Calculate total fare for a ride
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -15,10 +18,26 @@ public class InvoiceGeneratorTest {
 
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    //UC2 - For multiple rides
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare(){
+        RideDetail[] rideDetail = {new RideDetail(2.0,5),
+                                   new RideDetail(0.1,1)
+                                  };
+        double fare = invoiceGenerator.calculateTotalFare(rideDetail);
+        Assert.assertEquals(30, fare, 0.0);
+    }
+
+    @Test
+    public void shouldReturnMinFareForZeroDistanceAndTime(){
+        RideDetail[] rideDetail = {new RideDetail(0.0,0)};
+        double fare = invoiceGenerator.calculateTotalFare(rideDetail);
         Assert.assertEquals(5, fare, 0.0);
     }
 }
