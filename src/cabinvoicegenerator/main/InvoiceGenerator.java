1f6e4bc41591b622b286349694caf57d421c4403
dbasumatary <basudhondip@gmail.com>
2023-03-15 23:47:14 +0530

    Added: Method to calculate total fare for multiple rides

--- src/cabinvoicegenerator/main/InvoiceGenerator.java
+++ src/cabinvoicegenerator/main/InvoiceGenerator.java
@@ -6,10 +6,22 @@ public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
 
+    //UC1 - Calculate total fare for a ride
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
 
         //Return either totalFare or minimum fare whoever is greater
         return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    //UC2 - For multiple rides
+    public double calculateTotalFare(RideDetail[] rideDetail){
+        double totalFare = 0.0;
+        for (RideDetail ride : rideDetail) {
+
+            //Storing the aggregate total in an array
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
     }
 }
